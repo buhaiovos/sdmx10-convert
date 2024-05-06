7b7b09a4336729d0cdab91fb7b1c1514b9fc376b
Oleksandr Buhaiov <oleksandr_buhaiov@epam.com>
2024-05-06 23:13:31 +0300

    add parameter to specify dataset for conversion

--- convert_data.py
+++ convert_data.py
@@ -42,7 +42,9 @@ def mapTimePeriod(freq:str, period:str) -> str:
 
 class XmlToCsvConverter(xml.sax.ContentHandler):
     
-    def __init__(self, outfile:TextIOWrapper, normalizedPickle:BufferedReader):
+    def __init__(self, outfile:TextIOWrapper, 
+                 normalizedPickle:BufferedReader, 
+                 datasetId:str):
         xml.sax.ContentHandler.__init__(self)
         self.outfile = outfile
         self.firstRow = True
@@ -53,6 +55,8 @@ class XmlToCsvConverter(xml.sax.ContentHandler):
         self.currentFreq = ''
         self.header = []
         self.normalizedValues:set = pickle.load(normalizedPickle)
+        self.datasetId = datasetId
+        self.active = False
         print(self.normalizedValues)
         
     def processValue(self, value:str) -> str:
@@ -61,46 +65,57 @@ class XmlToCsvConverter(xml.sax.ContentHandler):
         return value
     
     def startElement(self, name, attrs):
-        if (name == 'kf:Series'):
-            for attrName in attrs.keys():
-                attrValue = attrs[attrName]
-                
-                if (attrName == 'FREQ'):
-                    self.currentFreq = attrValue
-                
-                if (self.firstRow):
-                    self.header.append(attrName)
-                self.currentSeries.append(self.processValue(attrValue))
-        
-        elif (name == 'frb:Obs'):
-            for attrName in attrs.keys():
-                attrValue = attrs[attrName]
-                
-                if (attrName == 'TIME_PERIOD'):
-                    self.currentTimePeriod = attrValue
-                
-                if (self.firstRow):
-                    self.header.append(attrName)
-                
-                self.currentObs.append(self.processValue(attrValue))
+        if (name == 'frb:DataSet' and attrs['id'] == self.datasetId):
+            self.active = True
+            
+        if (self.active):    
+            if (name == 'kf:Series'):
+                for attrName in attrs.keys():
+                    attrValue = attrs[attrName]
+                    
+                    if (attrName == 'FREQ'):
+                        self.currentFreq = attrValue
+                    
+                    if (self.firstRow):
+                        self.header.append(attrName)
+                    self.currentSeries.append(self.processValue(attrValue))
+            
+            elif (name == 'frb:Obs'):
+                for attrName in attrs.keys():
+                    attrValue = attrs[attrName]
+                    
+                    if (attrName == 'TIME_PERIOD'):
+                        self.currentTimePeriod = attrValue
+                    
+                    if (self.firstRow):
+                        self.header.append(attrName)
+                    
+                    self.currentObs.append(self.processValue(attrValue))
 
     
     def endElement(self, name):
-        if (name == 'frb:Obs'):
-            if(self.firstRow):
-                self.header.append('TIME_PERIOD_MAPPED')
-                self.csvWriter.writerow(self.header)
-                self.firstRow = False
-                self.header.clear()
+        if (self.active):
+            if (name == 'frb:DataSet'):
+                self.active = False
+                return
+            
+            if (name == 'frb:Obs'):
+                if(self.firstRow):
+                    self.header.append('TIME_PERIOD_MAPPED')
+                    self.csvWriter.writerow(self.header)
+                    self.firstRow = False
+                    self.header.clear()
+                    
+                mappedTimePeriod = mapTimePeriod(self.currentTimePeriod, self.currentFreq)
                 
-            mappedTimePeriod = mapTimePeriod(self.currentTimePeriod, self.currentFreq)
-            
-            self.currentObs.append(mappedTimePeriod)
-            self.csvWriter.writerow(self.currentSeries + self.currentObs)
-            self.currentObs.clear()
-            
-        if (name == 'kf:Series'):
-            self.currentSeries.clear()
+                self.currentObs.append(mappedTimePeriod)
+                self.csvWriter.writerow(self.currentSeries + self.currentObs)
+                self.currentObs.clear()
+                return
+                
+            if (name == 'kf:Series'):
+                self.currentSeries.clear()
+                return
 
     
     def characters(self, _):
@@ -111,12 +126,13 @@ def main():
     argumentsParser = argparse.ArgumentParser(description='Process XML file.')
     argumentsParser.add_argument('input_file', help='Input XML file')
     argumentsParser.add_argument('output_file', help='Output XML file')
+    argumentsParser.add_argument('dataset_id', help='Dataset ID')
     args = argumentsParser.parse_args()
 
     with (open(args.input_file, 'r') as input, 
           open(args.output_file, 'w', newline='') as out,
           open('normalized_values.pickle', 'rb') as normalizedPickle):
-        handler = XmlToCsvConverter(out, normalizedPickle)
+        handler = XmlToCsvConverter(out, normalizedPickle, args.dataset_id)
         parser = xml.sax.make_parser()
         parser.setContentHandler(handler)
         parser.parse(input)
